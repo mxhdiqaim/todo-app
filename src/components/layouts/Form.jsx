01337b92bd701c48ea65f72af5d69eae3bae2899
Mahdi <muhdmahdia36@gmail.com>
2022-03-27 14:38:52 +0100

    FOrm login/logout btn added, form datas collected

--- src/components/layouts/Form.jsx
+++ src/components/layouts/Form.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [todo, setTodo] = useState({
+    title: '',
+    isCompleted: false,
+    isPublic: false,
+  });
+
+  const { title, isPublic } = todo;
+
+  const onChange = e => {
+    setTodo({ ...todo, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(todo);
+  };
+
   return (
     <div className='form'>
-      <form>
-        <input type='text' name='search' placeholder='some words' />
-        <button type='submit'>+</button>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type='text'
+            name='title'
+            value={title}
+            onChange={onChange}
+            placeholder='some words'
+          />
+          <button type='submit'>+</button>
+        </div>
+        <div className='mim'>
+          <input
+            className='checkbox'
+            type='checkbox'
+            name='isPublic'
+            value={isPublic}
+            onChange={() => setTodo({ ...todo, isPublic: !isPublic })}
+          />
+          <label>Public Todo</label>
+        </div>
         <hr />
       </form>
     </div>
