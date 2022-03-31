bdf6efa28b688636393269368d2f7219e5f9560b
Mahdi <muhdmahdia36@gmail.com>
2022-03-31 12:27:35 +0100

    Notfound restyled, and some files clean ups

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './components/pages/NotFound';
 import Home from './components/pages/Home';
 
-// import PrivateRoute from './components/routing/PrivateRoute';
-
 // Context APIs
 import AlertState from './context/alert/AlertState';
-import TodoState from './context/todos/TodoState';
 
 import './App.scss';
-
-// TOKEN
-import setAuthToken from './utils/setAuthToken';
-
-if (localStorage.accessToken) {
-  setAuthToken(localStorage.accessToken);
-}
 
 const App = () => {
   return (
     <AlertState>
-      <TodoState>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='*' component={NotFound} />
-          </Switch>
-        </Router>
-      </TodoState>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
     </AlertState>
   );
 };
